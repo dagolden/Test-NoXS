aa717f1f974c14fd11ad629b940d876887964094
David Golden <dagolden@cpan.org>
2006-09-09 18:11:39 +0000

    r4039@vesta (orig r565):  david | 2006-06-09 03:53:11 -0400  [repos] tagging release-1.00
    git-svn-id: https://dagolden.googlecode.com/svn/Test-NoXS/tags/release-1.00@577 dfce27d5-b31c-0410-bb09-030b4413eeba

---
+++ lib/Test/NoXS.pm
@@ -0,0 +1,117 @@
+package Test::NoXS;
+
+$VERSION = "1.00";
+
+use strict;
+# use warnings; # only for Perl >= 5.6
+
+my @no_xs_modules;
+
+sub import {
+    my $class = shift;
+    push @no_xs_modules, @_;
+}
+    
+# Overload DynaLoader and XSLoader to fake lack of XS for designated modules
+{
+    no strict 'refs';
+    local $^W;
+    require DynaLoader;
+    my $bootstrap_orig = *{"DynaLoader::bootstrap"}{CODE};
+    *DynaLoader::bootstrap = sub {
+        die if grep { $_[0] eq $_ } @no_xs_modules;
+        goto $bootstrap_orig;
+    };
+    # XSLoader entered Core in Perl 5.6
+    if ( $] >= 5.006 ) {
+        require XSLoader;
+        my $xsload_orig = *{"XSLoader::load"}{CODE};
+        *XSLoader::load = sub {
+            die if grep { $_[0] eq $_ } @no_xs_modules;
+            goto $xsload_orig;
+        };
+    }
+}
+    
+
+1; #this line is important and will help the module return a true value
+
+__END__
+
+=head1 NAME
+
+Test::NoXS - Prevent a module from loading its XS code
+
+=head1 SYNOPSIS
+
+ # Note: XS for Scalar::Util is actually in List::Util
+ use Test::NoXS 'List::Util'; 
+ 
+ eval "use Scalar::Util qw( weaken )";
+ 
+ like( $@, qr/weak references/i, "Scalar::Util failed to load XS" );
+
+=head1 DESCRIPTION
+
+This modules hijacks L<DynaLoader> and L<XSLoader> to prevent them from loading
+XS code for designated modules.  This is intended to help test how modules
+react to missing XS, e.g. by dying with a message or by falling back to a
+pure-Perl alternative.
+
+=head1 USAGE
+
+Modules that should not load XS should be given as a list of arguments to C<use
+Test::NoXS>.
+
+=head1 BUGS
+
+Please report any bugs or feature using the CPAN Request Tracker.  
+Bugs can be submitted by email to C<bug-Test-NoXS@rt.cpan.org> or 
+through the web interface at 
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=Test-NoXS>
+
+When submitting a bug or request, please include a test-file or a patch to an
+existing test-file that illustrates the bug or desired feature.
+
+=head1 AUTHOR
+
+David A. Golden (DAGOLDEN)
+
+dagolden@cpan.org
+
+http://dagolden.com/
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2006 by David A. Golden
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
+
+=head1 DISCLAIMER OF WARRANTY
+
+BECAUSE THIS SOFTWARE IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
+FOR THE SOFTWARE, TO THE EXTENT PERMITTED BY APPLICABLE LAW. EXCEPT WHEN
+OTHERWISE STATED IN WRITING THE COPYRIGHT HOLDERS AND/OR OTHER PARTIES
+PROVIDE THE SOFTWARE "AS IS" WITHOUT WARRANTY OF ANY KIND, EITHER
+EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE. THE
+ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE SOFTWARE IS WITH
+YOU. SHOULD THE SOFTWARE PROVE DEFECTIVE, YOU ASSUME THE COST OF ALL
+NECESSARY SERVICING, REPAIR, OR CORRECTION.
+
+IN NO EVENT UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING
+WILL ANY COPYRIGHT HOLDER, OR ANY OTHER PARTY WHO MAY MODIFY AND/OR
+REDISTRIBUTE THE SOFTWARE AS PERMITTED BY THE ABOVE LICENCE, BE
+LIABLE TO YOU FOR DAMAGES, INCLUDING ANY GENERAL, SPECIAL, INCIDENTAL,
+OR CONSEQUENTIAL DAMAGES ARISING OUT OF THE USE OR INABILITY TO USE
+THE SOFTWARE (INCLUDING BUT NOT LIMITED TO LOSS OF DATA OR DATA BEING
+RENDERED INACCURATE OR LOSSES SUSTAINED BY YOU OR THIRD PARTIES OR A
+FAILURE OF THE SOFTWARE TO OPERATE WITH ANY OTHER SOFTWARE), EVEN IF
+SUCH HOLDER OR OTHER PARTY HAS BEEN ADVISED OF THE POSSIBILITY OF
+SUCH DAMAGES.
+
+=cut
---
+++ t/01_Test_NoXS.t
@@ -0,0 +1,35 @@
+# Test::NoXS tests
+use strict;
+
+use Test::More;
+
+plan tests =>  6;
+
+require_ok( 'Test::NoXS' );
+
+# Scalar::Util actually bootstraps List::Util
+eval "use Test::NoXS qw( List::Util DB_File)";
+
+is( $@, q{},  "told Test::NoXS not to load XS for Scalar::Util or DB_File" );
+
+my $use_SU = "use Scalar::Util qw( weaken )";
+
+eval $use_SU;
+
+ok( $@, "'$use_SU' threw an error" );
+
+like( $@, '/weak references/i', 
+    "error matched warning for unavailable weak references (i.e XS not loaded)"
+);
+
+my $use_F = "use Fcntl qw( LOCK_EX )";
+
+eval $use_F;
+
+is( $@, q{}, "'$use_F' successful" );
+
+ok( defined *main::LOCK_EX{CODE}, "function LOCK_EX imported (i.e. XS loaded)" );
+
+#silence warning
+LOCK_EX();
+
